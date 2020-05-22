22957daccda6d7f7fc2c5c37ddebd9f7c85b702a
tairy <tairyguo@gmail.com>
2020-05-22 15:32:37 +0800

    add restart command

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,4 @@ chmod 600 "$SSH_PATH/deploy_key"
 
 # Do deployment
 sh -c "rsync $FIRST_ARGS -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $SECOND_ARGS $GITHUB_WORKSPACE/ $THIRD_ARGS"
-
-if [[ !-z "${SERVER_HOST}" ]]; then
-	sh -c "ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no $SERVER_HOST '$RESTART_COMMAND'"
-fi
+sh -c "ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no $SERVER_HOST '$RESTART_COMMAND'"
