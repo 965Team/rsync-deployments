3cc80e50012d9ec1961f5f16da138bf5c8c66d08
tairy <tairyguo@gmail.com>
2020-05-22 15:21:35 +0800

    judge if php.

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,6 @@ chmod 600 "$SSH_PATH/deploy_key"
 # Do deployment
 sh -c "rsync $FIRST_ARGS -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $SECOND_ARGS $GITHUB_WORKSPACE/ $THIRD_ARGS"
 
-if [[ -z "${SERVER_HOST}" ]]; then
+if [[ !-z "${SERVER_HOST}" ]]; then
 	sh -c "ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no $SERVER_HOST '/usr/local/bin/php /home/working/XQ_Api/bin/swoft http:restart'"
 fi
