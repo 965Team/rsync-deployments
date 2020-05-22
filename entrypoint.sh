7a431f00c0159b4c1af64d89fa18fe360bdb477a
tairy <tairyguo@gmail.com>
2020-05-22 11:50:22 +0800

    reload swoft.

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,3 +11,4 @@ chmod 600 "$SSH_PATH/deploy_key"
 
 # Do deployment
 sh -c "rsync $FIRST_ARGS -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $SECOND_ARGS $GITHUB_WORKSPACE/ $THIRD_ARGS"
+sh -c "ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no '/usr/local/bin/php /home/working/XQ_Api/bin/swoft http:reload'"
